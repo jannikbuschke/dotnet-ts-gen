db5b0747bbc38601924507e77db46684c890b27b
Jannik <sisosys6@gmail.com>
2024-02-19 09:28:50 +0100

    add queryEndpointWithoutInput and queryEndpoint

--- src/TinyTypeGen.Giraffe/Library.fs
+++ src/TinyTypeGen.Giraffe/Library.fs
@@ -11,6 +11,26 @@ let endpoint<'request, 'response> (url: string) (verb: TsGen.HttpVerb) (handler:
     TsGen.ApiEndpoint.Route = url },
   handler
 
+let queryEndpointWithoutInput<'response, 'error> (url: string) (handler: HttpContext -> unit -> Task<Result<'response, 'error>>) =
+  endpoint<unit, 'response> url TsGen.HttpVerb.GET (fun next ctx ->
+    task {
+      let! response = handler ctx ()
+      match response with
+      | Result.Ok result -> return! json result next ctx
+      | Result.Error error -> return! json error next ctx
+    })
+
+let queryEndpoint<'request, 'response, 'error> (url: string) (handler: HttpContext -> 'request -> Task<Result<'response, 'error>>) =
+  endpoint<'request, 'response> url TsGen.HttpVerb.GET (fun next ctx ->
+    task {
+      let request = ctx.BindQueryString<'request>()
+      let! response = handler ctx request
+
+      match response with
+      | Result.Ok result -> return! json result next ctx
+      | Result.Error error -> return! json error next ctx
+    })
+
 let mutationEndpoint<'request, 'response, 'error> (url: string) (handler: HttpContext -> 'request -> Task<Result<'response, 'error>>) =
   endpoint<'request, 'response> url TsGen.HttpVerb.POST (fun next ctx ->
     task {
